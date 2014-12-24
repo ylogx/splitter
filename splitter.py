dcb92ed7aa154b1684a30b644b41361bc7175d9c
Shubham Chaudhary <me@shubhamchaudhary.in>
2014-12-25 04:04:08 +0530

    Divide by size, take care of missing subdirectories in subfolders

--- splitter.py
+++ splitter.py
@@ -3,12 +3,14 @@ import os
 import sys
 
 def init_dir(directory):
+    print('Given', directory)
     if not os.path.exists(directory):
         print('Creating directory:', directory)
         os.makedirs(directory)
 
 def get_output_directory(file):
     extension = file.split('.')[-1]
+    file_size = os.path.getsize(file)
     output_dir = None
     if extension in ['jpg', 'jpeg', 'png', 'tiff', 'bmp']:
         output_dir = 'images'
@@ -16,20 +18,30 @@ def get_output_directory(file):
         output_dir = 'code'
     elif extension in ['mp4', 'mkv', 'avi', 'flv', '3gp', 'mov']:
         output_dir = 'videos'
+    elif extension in ['mp3', 'm4a']:
+        output_dir = 'music'
+    elif file_size > 0 and file_size <= 10000:
+        output_dir = 'ultra-small'
+    elif file_size > 10001 and file_size <= 100000:
+        output_dir = 'small'
+    elif file_size > 100001 and file_size <= 10000000:
+        output_dir = 'medium'
+    elif file_size > 10000001:
+        output_dir = 'large'
     return output_dir
 
 def make_the_move(output_dir, filename):
     init_dir(output_dir)
     src = filename
     dest = os.path.join(output_dir, filename)
+    init_dir(os.path.dirname(dest))
     os.rename(src, dest)
-    print('Moved', src, 'to', dest)
+    print('   ', 'Moved', src, 'to', dest)
 
 def splitter(in1, in2):
     for (dirpath, dirnames, filenames) in os.walk(in1):
-        if (dirpath == './images'
-                or dirpath == './videos'
-                or dirpath == './code'):
+        if (dirpath in ['./images', './videos', './code', './ultra-small',
+                        './small', './medium', './large']):
             print('Skipping ', dirpath)
             continue
         #print(dirpath, dirnames, filenames)
@@ -41,7 +53,7 @@ def splitter(in1, in2):
             print('    ', filename_count, '. ', sep='', end='')
             filename_count -= 1
             filename = os.path.join(dirpath, file)
-            output_dir = get_output_directory(file)
+            output_dir = get_output_directory(filename)
             if output_dir:
                 print('Operating on', filename)
                 make_the_move(output_dir, filename)
